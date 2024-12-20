30a754f2aff07aaadd65065a23029f0ed6e80575
FishShalami <fishshalami@gmail.com>
2024-12-19 23:01:03 -0800

    Added function for displaying library, styling of lib cards

--- script.js
+++ script.js
@@ -17,3 +17,34 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
+const library = document.querySelector(".library");
+
+
+
+
+function libraryLoop(myLibrary) {
+    for (let book of myLibrary) {
+        //  console.log(book.title);
+         const libraryCard = document.createElement('div');
+         libraryCard.classList.add('libraryCard');
+         library.appendChild(libraryCard);
+         const cardTitle = document.createElement('p');
+         cardTitle.classList.add('bookTitle');
+         cardTitle.innerText = book.title;
+         libraryCard.appendChild(cardTitle);
+         const cardAuthor = document.createElement('p');
+         cardAuthor.classList.add('bookAuth');
+         cardAuthor.innerText = book.author;
+         libraryCard.appendChild(cardAuthor);
+    };
+}
+
+
+addBookToLibrary("Another Book!", "Sean Connery", "200 pages", "read");
+
+addBookToLibrary("More Books", "Tim Letterman", "100 pages", "not read");
+
+addBookToLibrary("Book!", "Bob Jones", "300 pages", "read");
+
+addBookToLibrary("Mockingbird", "Sean Jonathan", "900 pages", "not read");
+
