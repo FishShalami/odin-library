b1c86eeea7f6ec381ddc2a9e3ea021abbe8e73e2
FishShalami <fishshalami@gmail.com>
2024-12-20 16:05:21 -0800

    Functioning dialog for new book into library card

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ Book.prototype.info = function() {
 function addBookToLibrary(title, author, pages, read) {
     let  newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+    libraryLoop();
 }
 
 const library = document.querySelector(".library");
@@ -22,42 +23,75 @@ const library = document.querySelector(".library");
 
 
 
-function libraryLoop(myLibrary) {
+function libraryLoop() {
+    library.innerHTML = ""; // Clear existing content
     for (let book of myLibrary) {
-        //  console.log(book.title);
-         const libraryCard = document.createElement('div');
-         libraryCard.classList.add('libraryCard');
-         library.appendChild(libraryCard);
-         const cardTitle = document.createElement('p');
-         cardTitle.classList.add('bookTitle');
-         cardTitle.innerText = book.title;
-         libraryCard.appendChild(cardTitle);
-         const cardAuthor = document.createElement('p');
-         cardAuthor.classList.add('bookAuth');
-         cardAuthor.innerText = book.author;
-         libraryCard.appendChild(cardAuthor);
-    };
+        const libraryCard = document.createElement('div');
+        libraryCard.classList.add('libraryCard');
+        
+        const cardTitle = document.createElement('p');
+        cardTitle.classList.add('bookTitle');
+        cardTitle.innerText = `Title: ${book.title}`;
+        
+        const cardAuthor = document.createElement('p');
+        cardAuthor.classList.add('bookAuth');
+        cardAuthor.innerText = `Author: ${book.author}`;
+
+        const cardPages = document.createElement('p');
+        cardPages.classList.add('bookPages');
+        cardPages.innerText = `Pages: ${book.pages}`;
+
+        const cardRead = document.createElement('p');
+        cardRead.classList.add('bookRead');
+        cardRead.innerText = `Read: ${book.read === "read" ? "Yes" : "No"}`;
+
+        libraryCard.append(cardTitle, cardAuthor, cardPages, cardRead);
+        library.appendChild(libraryCard);
+    }
 }
+
+const dialog = document.querySelector('dialog')
+
+document.querySelector('button.newBook').addEventListener('click', () => {
+    dialog.showModal();
+}); 
+
+document.querySelector('button.dialogClose').addEventListener("click", (e) => {
+    e.preventDefault(); //allows close even if fields empty
+    dialog.close();
+});
+
+document.querySelector('form[name="addBook"]').addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent form from reloading page
+    
+    const form = event.target;
+    const title = form.title.value.trim();
+    const author = form.author.value.trim();
+    const pages = form.pages.value.trim();
+    const read = form.readYN.value;
+
+    if (title && author && pages) {
+        addBookToLibrary(title, author, pages, read);
+        form.reset(); // Reset form fields
+        document.querySelector('dialog').close(); // Close dialog
+    } else {
+        alert("Please fill in all fields.");
+    }
+});
 
 
 addBookToLibrary("Another Book!", "Sean Connery", "200 pages", "read");
 
-addBookToLibrary("More Books", "Tim Letterman", "100 pages", "not read");
+addBookToLibrary("More Books", "Tim Letterman", "100 pages", "notRead");
 
 addBookToLibrary("Book!", "Bob Jones", "300 pages", "read");
 
-addBookToLibrary("Mockingbird", "Sean Jonathan", "900 pages", "not read");
+addBookToLibrary("Mockingbird", "Sean Jonathan", "900 pages", "notRead");
 
-libraryLoop(myLibrary) 
+addBookToLibrary("More Books", "Tim Letterman", "100 pages", "notRead");
 
-const newBookButton = document.querySelector('button.newBook');
-const dialog = document.querySelector('dialog');
-const closeButton = document.querySelector('button.dialogClose');
+addBookToLibrary("Book!", "Bob Jones", "300 pages", "read");
 
-newBookButton.addEventListener('click', () => {
-    dialog.showModal();
-}); 
+addBookToLibrary("Mockingbird", "Sean Jonathan", "900 pages", "notRead");
 
-closeButton.addEventListener("click", () => {
-    dialog.close();
-  });
+libraryLoop(); 
